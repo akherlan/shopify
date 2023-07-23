969317466056e6c135a7a78b5fba78a9101e85d5
Andi <getdata@duck.com>
2023-07-23 14:07:50 +0700

    scraper simplifying (#7)
    - using products.json (better scraper)
    - listing shop.txt
    - include product_id, variant_id
    - remove currency, condition

--- README.md
+++ README.md
@@ -2,8 +2,19 @@
 
 Indonesia local brand:
 
-- [En-ji](https://enji.co.id/)
-- [Et cetera](https://etcetera.co.id/)
+- [Buttonscarves](https://buttonscarves.com/)
+- [Color Box](https://colorbox.co.id/)
+- [Dian Pelangi](https://dpstudio.co/)
+- [Elzatta](https://elzatta.com/)
+- [En-Ji](https://enji.co.id/)
+- [Erigo](https://erigostore.co.id/)
+- [Et Cetera](https://etcetera.co.id/)
+- [Executive] (https://executive.co.id/)
+- [Geela](https://geela.id/)
 - [Kami](https://kamiidea.com/)
+- [Major Minor](https://shop.majorminorstore.com/)
+- [Minimal](https://minimal.id/)
 - [Palomino](https://palominobag.com/)
 - [Puru Kambera](https://purukambera.com/)
+- [Valino](https://valino.co.id/)
+- [Zaskia Mecca](https://www.zmnow.id/) / ZM
--- scraper.py
+++ scraper.py
@@ -1,70 +1,151 @@
 import httpx
-import asyncio
-
-from random import randint
-from urllib.parse import urljoin
-from selectolax.parser import HTMLParser
-
-import logging
 import json
-import os
-import re
+import pandas as pd
+from urllib.parse import urljoin
+from datetime import datetime, timedelta, timezone
+import logging
 
 
 logging.basicConfig(
-    # filename="log.log",
     level=logging.INFO,
     format="%(asctime)s [%(levelname)s] %(message)s",
     datefmt="%Y-%m-%d %H:%M:%S",
 )
 
 
-def parse_product(response):
-    html = HTMLParser(response.text)
-    try:
-        json_obj = html.css("script[type='application/json']")
-        if len(json_obj) >= 2:
-            product = json_obj[1]
-        elif html.css_matches("link[type='application/json+oembed']"):
-            json_obj = list(
-                filter(lambda node: node.css_matches("script[id*=product]"), json_obj)
-            )
-            product = json_obj[0] if len(json_obj) > 0 else None
+class ShopifyScraper:
+    def __init__(self, website, **kwargs):
+        headers = {"user-agent": "httpx"}
+        self.session = httpx.Client(timeout=30, headers=headers, **kwargs)
+        self.website = website
+
+    def fetch(self, limit=None):
+        url = urljoin(self.website, "products.json")
+
+        if limit is None:
+            limit = 999999
+            lim = 250
+        elif limit > 250:
+            lim = 250
         else:
-            product = html.css_first("script[type='application/ld+json']")
-        if product:
-            product = product.text(strip=True)
-            product = json.loads(product, strict=False)
-    except Exception as e:
-        logging.info("title: {}".format(html.css_first("title").text(strip=True)))
-        logging.error(f"{str(e)} {str(response.url)}", exc_info=True)
-        product = None
-    finally:
-        yield product
+            lim = limit
 
-
-async def get_product(urlpath: str, timeout: int = 60):
-    async with httpx.AsyncClient(timeout=httpx.Timeout(timeout)) as session:
-        with open(urlpath) as fin:
-            links = [link.strip() for link in fin.readlines()]
-        requests = [session.get(link) for link in links]
-        for task in asyncio.as_completed(requests):
-            response = await task
-            await asyncio.sleep(randint(0, 5))
-            for product in parse_product(response):
-                yield product
-
-
-async def get_data(fileout, filein):
-    if re.findall("/", fileout):
-        directory = fileout.replace(fileout.split("/")[-1], "")
-        if not os.path.exists(directory):
-            os.makedirs(directory)
-    with open(fileout, "w") as f:
+        p = 1
+        last = False
         data = []
-        async for product in get_product(filein):
-            data.append(product)
-        json.dump(data, f)
+
+        while not last:
+            par = {"limit": lim, "page": p}
+            response = self.session.get(url, params=par)
+            logging.info("GET page {}".format(str(p)))
+            if response.status_code == 200:
+                rawjson = response.json().get("products")
+                data += rawjson
+                last = bool(not len(rawjson)) or len(data) >= limit
+                p += 1
+            else:
+                logging.error("bad response {}".format(response.status_code))
+                return
+
+        if len(data) > limit:
+            data = data[:limit]
+        logging.info("collect {} items from {}".format(len(data), self.website))
+        return data
+
+    def convert_date(self, string, fmt):
+        return datetime.strptime(string, fmt)
+
+    def transform(self, rawdata):
+        date_fmt = "%Y-%m-%dT%H:%M:%S%z"
+        tzinfo = timezone(timedelta(hours=7))
+        # acquisition_date = datetime.now(tzinfo).strftime(date_fmt)
+        acquisition_date = datetime.now(tzinfo).replace(microsecond=0).isoformat()
+
+        name = list(map(lambda item: item.get("title"), rawdata))
+        product_id = list(map(lambda item: item.get("id"), rawdata))
+        description = list(map(lambda item: item.get("body_html"), rawdata))
+        brand = list(map(lambda item: item.get("vendor"), rawdata))
+        category = list(map(lambda item: item.get("product_type"), rawdata))
+        date_release = list(map(lambda item: item.get("published_at"), rawdata))
+        slug = list(map(lambda item: item.get("handle"), rawdata))
+        # images = list(map(lambda item: item.get("images"), rawdata))
+        category = list(map(lambda item: item.get("product_type"), rawdata))
+        tag = list(map(lambda item: item.get("tags"), rawdata))
+
+        variants = list(map(lambda item: item.get("variants"), rawdata))
+        collectible_product, collectible_offers = [], []
+
+        for i, var in enumerate(variants):
+            varid = list(map(lambda item: item.get("id"), var))
+            varname = list(map(lambda item: item.get("title"), var))
+            sku = list(map(lambda item: item.get("sku"), var))
+
+            is_instock = list(map(lambda item: item.get("available"), var))
+            price = list(map(lambda item: int(float(item.get("price"))), var))
+            # is_discount = list(
+            #     map(lambda item: item.get("compare_at_price") != item.get("price"), var)
+            # )
+
+            product = pd.DataFrame(
+                list(
+                    zip(
+                        [product_id[i]] * len(sku),
+                        sku,
+                        [name[i]] * len(sku),
+                        [brand[i]] * len(sku),
+                        [category[i]] * len(sku),
+                        varid,
+                        varname,
+                        [date_release[i]] * len(sku),
+                        [description[i]] * len(sku),
+                        [slug[i]] * len(sku),
+                    )
+                ),
+                columns=[
+                    "product_id",
+                    "sku",
+                    "name",
+                    "brand",
+                    "category",
+                    "variant_id",
+                    "variant_name",
+                    "date_release",
+                    "description",
+                    "slug",
+                ],
+            )
+            collectible_product.append(product)
+
+            offers = pd.DataFrame(
+                list(
+                    zip(
+                        [product_id[i]] * len(sku),
+                        varid,
+                        sku,
+                        price,
+                        is_instock,
+                        [acquisition_date] * len(sku),
+                        [self.website] * len(sku),
+                    )
+                ),
+                columns=[
+                    "product_id",
+                    "variant_id",
+                    "sku",
+                    "price",
+                    "is_instock",
+                    "date_acquisition",
+                    "source",
+                ],
+            )
+            collectible_offers.append(offers)
+
+        product = pd.concat(collectible_product, ignore_index=True)
+        offers = pd.concat(collectible_offers, ignore_index=True)
+        return product, offers
+
+    def save(self, dataset, fname):
+        dataset.to_csv(fname, index=False)
 
 
 if __name__ == "__main__":

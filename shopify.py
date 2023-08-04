0bccbf287a8fa6f332c4d4b2c9421835db5fb4cf
Andi <getdata@duck.com>
2023-08-04 11:28:40 +0700

    input-output handle from args

--- shopify.py
+++ shopify.py
@@ -5,17 +5,20 @@ import argparse
 import time
 
 
-def main(url, product_file, offers_file):
+def main(url, product_file=None, offers_file=None, timer=False):
     tic = time.perf_counter()
-
     shopify = ShopifyScraper(url)
     data = shopify.fetch()
     product, offers = shopify.transform(data)
-    shopify.save(product, product_file)
-    shopify.save(offers, offers_file)
-
-    toc = time.perf_counter()
-    print(f"---------- {toc - tic:.2f} seconds ----------")
+    if product_file is not None:
+        shopify.save(product, product_file)
+        print(f"Saved product dataset to {product_file}")
+    if offers_file is not None:
+        shopify.save(offers, offers_file)
+        print(f"Saved pricing data to {offers_file}")
+    if timer:
+        toc = time.perf_counter()
+        print(f"---------- {toc - tic:.2f} seconds ----------")
 
 
 if __name__ == "__main__":
@@ -23,7 +26,15 @@ if __name__ == "__main__":
         description="Scraping product from a Shopify powered website"
     )
     parser.add_argument("url", help="Shopify website")
-    parser.add_argument("-p", "--product", help="product file output")
-    parser.add_argument("-o", "--offers", help="prices file output")
+    parser.add_argument("-p", "--product", nargs="?", help="Product file output")
+    parser.add_argument("-o", "--offers", nargs="?", help="Prices file output")
+    parser.add_argument(
+        "-t", "--timer", action="store_true", help="Show timer at the end"
+    )
     args = parser.parse_args()
-    main(url=args.url, product_file=args.product, offers_file=args.offers)
+    main(
+        url=args.url,
+        product_file=args.product,
+        offers_file=args.offers,
+        timer=args.timer,
+    )
